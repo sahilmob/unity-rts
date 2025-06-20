a9d2fb6cb0c8aa512d6c69a6f8ee6bbf02730db9
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-06-20 17:14:27 +0300

    camera zoom

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,12 +1,77 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Cinemachine;
+using System;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private Transform cameraTarget;
+    [SerializeField] private CinemachineCamera cinemachineCamera;
     [SerializeField] private float keyboardPanSpeed = 5;
+    [SerializeField] private float zoomSpeed = 1;
+    [SerializeField] private float minZoomDistance = 7.5f;
+    private CinemachineFollow cinemachineFollow;
+    private float zoomStartTime;
+    private Vector3 startingFollowOffset;
+
+    private void Awake()
+    {
+        if (!cinemachineCamera.TryGetComponent(out cinemachineFollow))
+        {
+            Debug.LogError("Cinemachine camera didn't have CinemachineFollow. Zoom will not work!");
+        }
+
+        startingFollowOffset = cinemachineFollow.FollowOffset;
+    }
 
     private void Update()
+    {
+        HandlePanning();
+        HandleZooming();
+    }
+
+    private void HandleZooming()
+    {
+        if (ShouldSetZoomStartTime())
+        {
+            zoomStartTime = Time.time;
+        }
+
+        Vector3 targetFollowOffset;
+
+        float zoomTime = Mathf.Clamp01((Time.time - zoomStartTime) * zoomSpeed);
+
+        if (Keyboard.current.altKey.isPressed)
+        {
+            targetFollowOffset = new Vector3(
+            cinemachineFollow.FollowOffset.x,
+            minZoomDistance,
+            cinemachineFollow.FollowOffset.z
+            );
+        }
+        else
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                startingFollowOffset.y,
+                cinemachineFollow.FollowOffset.z
+            );
+        }
+
+        cinemachineFollow.FollowOffset = Vector3.Slerp(
+             cinemachineFollow.FollowOffset,
+             targetFollowOffset,
+             zoomTime
+        );
+    }
+
+    private bool ShouldSetZoomStartTime()
+    {
+        return Keyboard.current.altKey.wasPressedThisFrame || Keyboard.current.altKey.wasReleasedThisFrame;
+
+    }
+
+    private void HandlePanning()
     {
         Vector2 moveAmount = Vector2.zero;
 
