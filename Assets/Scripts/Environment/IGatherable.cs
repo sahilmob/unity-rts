cb17528f23b47ac28a5bc668ab47b53fcb1b426d
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-06-26 18:34:37 +0300

    setup gatherable supplies

---
+++ Assets/Scripts/Environment/IGatherable.cs
@@ -0,0 +1,13 @@
+
+namespace RTS.Environment
+{
+    public interface IGatherable
+    {
+        public SupplySO Supply { get; }
+        public int Amount { get; }
+        public bool IsBusy { get; }
+
+        public bool BeginGather();
+        public int EndGather();
+    }
+}
