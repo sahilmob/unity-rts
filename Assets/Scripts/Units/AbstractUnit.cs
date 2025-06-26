904620f1c2260463ae7d23431b6447c97033ff44
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2025-06-26 17:18:12 +0300

    handle moving units using behavior tree

--- Assets/Scripts/Units/AbstractUnit.cs
+++ Assets/Scripts/Units/AbstractUnit.cs
@@ -2,25 +2,28 @@
 
 using RTS.EventBus;
 using RTS.Events;
+using Unity.Behavior;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace RTS.Units
 {
-    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(NavMeshAgent), typeof(BehaviorGraphAgent))]
     public abstract class AbstractUnit : AbstractCommandable, IMovable
     {
         public float AgentRadius => agent.radius;
         private NavMeshAgent agent;
+        private BehaviorGraphAgent graphAgent;
 
         public void MoveTo(Vector3 position)
         {
-            agent.SetDestination(position);
+            graphAgent.SetVariableValue("TargetLocation", position);
         }
 
         private void Awake()
         {
             agent = GetComponent<NavMeshAgent>();
+            graphAgent = GetComponent<BehaviorGraphAgent>();
         }
 
         protected override void Start()
